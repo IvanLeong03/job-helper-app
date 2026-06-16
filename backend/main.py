7c845724942fdbebf7b74d4029e1e429b82e4738
IvanLeong03 <quantumoopsie@gmail.com>
2026-06-16 11:27:07 +0100

    removed extra slash for cors origin

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173", 
-                   "https://job-helper-app.vercel.app/"],
+                   "https://job-helper-app.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
